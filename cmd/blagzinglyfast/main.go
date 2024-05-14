60feb37e649c67f28ddc53e062336d4790508235
Suanlianmang <40178928+Suanlianmang@users.noreply.github.com>
2024-05-14 23:09:02 +0530

    inital commit

---
+++ cmd/blagzinglyfast/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+    templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+    return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTempalte() *Templates {
+    return &Templates{
+        templates: template.Must(template.ParseGlob("views/*.html")),
+    }
+}
+
+type Count struct {
+    Count int
+}
+
+func main() {
+    e := echo.New()
+    e.Use(middleware.Logger())
+    e.Static("/static", "assets")
+    
+    count := Count{ Count: 0 }
+    e.Renderer = newTempalte()
+    e.GET("/", func(c echo.Context) error {
+        return c.Render(http.StatusOK, "index", count)
+    })
+
+    e.POST("/count", func(c echo.Context) error {
+        count.Count += 1
+        return c.Render(http.StatusOK, "count", count)
+    })
+   
+    e.Logger.Fatal(e.Start(":8000"))
+
+}
